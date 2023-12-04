d25cec1c9847cdaa07d1bf0574ab24a4c88f4a98
sangchu <didi1484@gmail.com>
2023-12-05 08:00:04 +0900

    feat: 서비스 시작문구 출력 추가

--- src/main/java/menu/controller/Controller.java
+++ src/main/java/menu/controller/Controller.java
@@ -1,7 +1,9 @@
 package menu.controller;
 
+import menu.view.OutputView;
+
 public class Controller {
     public void start() {
-
+        OutputView.printStartNotice();
     }
 }
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,8 +1,13 @@
 package menu.view;
 
 public class OutputView {
+    private static final String START_NOTICE = "점심 메뉴 추천을 시작합니다.";
+
     public static void printError(Exception e) {
         System.out.println(e.getMessage());
     }
 
+    public static void printStartNotice() {
+        System.out.println(START_NOTICE);
+    }
 }
