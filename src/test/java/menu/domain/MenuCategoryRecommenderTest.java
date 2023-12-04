fb7fd802b968e766fc9b7c8a8927f0256444101f
sangchu <didi1484@gmail.com>
2023-12-05 07:58:03 +0900

    test: 메뉴 카테고리 추천 테스트 추가

--- src/test/java/menu/domain/MenuCategoryRecommenderTest.java
+++ src/test/java/menu/domain/MenuCategoryRecommenderTest.java
@@ -1,11 +1,16 @@
 package menu.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 class MenuCategoryRecommenderTest {
 
     @Test
     void recommendMenuCategory() {
-        MenuCategoryRecommender.recommendMenuCategory();
+        MenuCategory menuCategory = MenuCategoryRecommender.recommendMenuCategory(1);
+        assertThat(menuCategory.getCategoryName()).isEqualTo("일식");
+        menuCategory = MenuCategoryRecommender.recommendMenuCategory(5);
+        assertThat(menuCategory.getCategoryName()).isEqualTo("양식");
     }
 }
