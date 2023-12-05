d9d1737c5c19c8eb346983589adaaec5231e8ae0
sangchu <didi1484@gmail.com>
2023-12-05 09:27:17 +0900

    test: 메뉴이름 검증 기능 추가

---
+++ src/test/java/menu/domain/IntolerantFoodTest.java
@@ -0,0 +1,18 @@
+package menu.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class IntolerantFoodTest {
+    @Test
+    void createIntolerantFood() {
+        List<String> foods1 = List.of("고기");
+        assertThatThrownBy(() -> new IntolerantFood(foods1)).isInstanceOf(IllegalArgumentException.class);
+
+        List<String> foods2 = List.of("우동", "스시");
+        assertThatCode(() -> new IntolerantFood(foods2)).doesNotThrowAnyException();
+    }
+}
